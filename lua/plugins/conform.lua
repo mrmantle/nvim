4f9b4b29786665ae8a7471cc5c63f7dd243f8d69
matthew.mantle <matthew.mantle@tribalgroup.com>
2025-04-11 13:29:20 +0100

    Add htmlangular to formatting filetypes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -28,6 +28,7 @@ return {
       }
     end,
     formatters_by_ft = {
+      htmlangular = { 'prettierd' },
       css = { 'prettierd' },
       json = { 'prettierd' },
       html = { 'prettierd' },
