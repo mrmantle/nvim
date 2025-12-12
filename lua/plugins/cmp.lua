2b372499452726143e9f9f29cfa85836e8d7eb6e
matthew.mantle <matthew.mantle@tribalgroup.com>
2025-12-12 09:58:01 +0000

    Remove LuaSnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -3,20 +3,7 @@ return {
   event = 'VimEnter',
   version = '1.*',
   dependencies = {
-    {
-      'L3MON4D3/LuaSnip',
-      version = '2.*',
-      build = 'make install_jsregexp',
-      dependencies = {
-        {
-          'rafamadriz/friendly-snippets',
-          config = function()
-            require('luasnip.loaders.from_vscode').lazy_load()
-          end,
-        },
-      },
-      opts = {},
-    },
+    'rafamadriz/friendly-snippets',
     'folke/lazydev.nvim',
   },
   ---@module 'blink.cmp'
