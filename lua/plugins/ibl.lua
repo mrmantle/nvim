712c7da82ed7ef09b658c45915ea5ef9a00a0f1c
matthew.mantle <matthew.mantle@tribalgroup.com>
2026-03-16 14:53:02 +0000

    Use different indent line char

--- lua/plugins/ibl.lua
+++ lua/plugins/ibl.lua
@@ -2,7 +2,7 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   config = function()
-    local indent_char = "▏"
+    local indent_char = "|"
     require("ibl").setup({
       indent = { char = indent_char },
       scope = {
