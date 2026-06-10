b5caaee4362a525928317c11e7e6b16fcd399eeb
matthew.mantle <matthew.mantle@tribalgroup.com>
2026-06-10 11:39:55 +0100

    Update diff options

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -37,6 +37,8 @@ vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 
+vim.o.diffopt = "internal,filler,closeoff,algorithm:histogram,indent-heuristic,inline:char,linematch:60"
+
 vim.diagnostic.config({
   virtual_text = false,
   float = {
